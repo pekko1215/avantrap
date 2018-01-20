fcf69112e8fc3a35ae3b7092a95143a6df5c9e9a
pekko1215 <pekko1215@yahoo.co.jp>
2018-01-21 01:00:29 +0900

    dwadadw

--- trap.js
+++ trap.js
@@ -1,10 +1,26 @@
 const sounder = new Sounder();
-const SoundList = ['カンニング.mp3','いくぞー.wav','さあいこかみんな.wav','最後の勝負.wav']
+const SoundList = [
+	'カンニング.mp3',
+	'いくぞー.wav',
+	'さあいこかみんな.wav',
+	'最後の勝負.wav',
+	'はじまったぞ.wav',
+	'ほらきたぞ.wav',
+	'みてるか.wav',
+	'奇跡.wav',
+	'どうだよ.wav',
+	'ほらな.wav'
+	];
+const MAX_TIME = 1000*60*2;
+var counter = MAX_TIME;
 SoundList.forEach((name,i)=>{
 	sounder.addFile("random/"+name, i);
 })
-sounder.loadFile(()=>{	
-	window.addEventListener('load',()=>{
-		sounder.play(0)
-	})
+sounder.loadFile(function(){
+	(function(){
+		sounder.playSound(Math.floor(Math.random()*SoundList.length),false,()=>{
+			arguments.callee()
+		});
+	})()
 })
+window.sounder = sounder
