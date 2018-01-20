5d1b5d65c5be7782585f22ed6d84e13460574957
pekko1215 <pekko1215@yahoo.co.jp>
2018-01-21 00:33:43 +0900

    dwa

--- trap.js
+++ trap.js
@@ -1,7 +1,7 @@
 const sounder = new Sounder();
 const SoundList = ['カンニング.mp3','いくぞー.wav','さあいこかみんな.wav','最後の勝負.wav']
 SoundList.forEach((name,i)=>{
-	sounder.addFile("ramdom/"+name, i);
+	sounder.addFile("random/"+name, i);
 })
 sounder.loadFile(()=>{	
 	window.addEventListener('load',()=>{
